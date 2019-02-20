12571df5cd07dee455d85a9a4110ba900c2acbc8
Gaby <gaby.a.ramos@gmail.com>
2019-02-20 08:23:02 -0600

    Agregando Parte usuario -Gaby

---
+++ resources/Gaby/private_area/inside/core/tab.php
@@ -0,0 +1,157 @@
+<?php
+class tab{
+
+	    public static function main(){
+        print('
+        <!DOCTYPE html>
+		<html lang="en">
+		<head>
+		  <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		  <meta http-equiv="x-ua-compatible" content="ie=edge">
+		  <title>Catálogo</title>
+		  <!-- Font Awesome - Social Icons -->
+		  <link href="css/all.css" rel="stylesheet">
+		  <!-- Navbar Style -->
+		  <link href="css/navsty.css" rel="stylesheet">
+		  <!-- Design -->
+		  <link href="css/design.css" rel="stylesheet">
+		  <!-- Transparent field-->
+		  <link href="css/field.css" rel="stylesheet">
+		  <!-- BG -->
+		  <link rel="stylesheet" href="css/BG.css">
+		</head>
+
+		<body>
+
+		  <!-- Navbar -->
+		  <nav class="navbar fixed-top navbar-expand-lg navbar-dark mdb-color scrolling-navbar">
+		    <div class="container">
+
+		      <!-- Brand -->
+		      <a class="navbar-brand" href="#" target="_blank">
+		        <strong>PopMovies</strong>
+		      </a>
+
+		      <!-- Collapse -->
+		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+		        aria-expanded="false" aria-label="Toggle navigation">
+		        <span class="navbar-toggler-icon"></span>
+		      </button>
+
+		      <!-- Links -->
+		      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+		        <!-- Left -->
+		        <ul class="navbar-nav mr-auto">
+		          <li class="nav-item active">
+		            <a class="nav-link" href="#">Catálogo
+		              <span class="sr-only">(current)</span>
+		            </a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link" href="#" target="_blank">Acerca de</a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link" href="#" target="_blank">Contáctanos</a>
+		          </li>
+		          <li class="nav-item">
+		            <a class="nav-link" href="#" target="_blank">Preguntas frecuentes</a>
+		          </li>
+		        </ul>
+
+		        <!-- Right -->
+		        <ul class="navbar-nav nav-flex-icons">
+		          <li class="nav-item">
+		            <a href="#" class="nav-link"
+		              target="_blank">
+		              <i class="#"></i>Cerrar Sesión
+		            </a>
+		          </li>
+		        </ul>
+
+		      </div>
+
+		    </div>
+		  </nav>
+		  <!-- Navbar -->
+        ');
+    }
+
+    public static function views(){
+    	print('
+    		<!DOCTYPE html>
+			<html lang="en">
+
+			<head>
+			  <meta charset="utf-8">
+			  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			  <meta http-equiv="x-ua-compatible" content="ie=edge">
+			  <title>Confirmar compra</title>
+			  <!-- Font Awesome - Social Icons -->
+			  <link href="css/all.css" rel="stylesheet">
+			  <!-- Navbar Style -->
+			  <link href="css/navsty.css" rel="stylesheet">
+			  <!-- Design -->
+			  <link href="css/design.css" rel="stylesheet">
+			  <!-- Transparent field-->
+			  <link href="css/field.css" rel="stylesheet">
+			</head>
+
+			<body>
+
+			  <!-- Navbar -->
+			  <nav class="navbar fixed-top navbar-expand-lg navbar-dark mdb-color scrolling-navbar">
+			    <div class="container">
+
+			      <!-- Brand -->
+			      <a class="navbar-brand" href="#" target="_blank">
+			        <strong>PopMovies</strong>
+			      </a>
+
+			      <!-- Collapse -->
+			      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+			        aria-expanded="false" aria-label="Toggle navigation">
+			        <span class="navbar-toggler-icon"></span>
+			      </button>
+
+			      <!-- Links -->
+			      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+			        <!-- Left -->
+			        <ul class="navbar-nav mr-auto">
+			          <li class="nav-item active">
+			            <a class="nav-link" href="home-page.php">Catálogo
+			              <span class="sr-only">(current)</span>
+			            </a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="#" target="_blank">Acerca de</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="https://mdbootstrap.com/docs/jquery/getting-started/download/" target="_blank">Contáctanos</a>
+			          </li>
+			          <li class="nav-item">
+			            <a class="nav-link" href="https://mdbootstrap.com/education/bootstrap/" target="_blank">Preguntas frecuentas</a>
+			          </li>
+			        </ul>
+
+			        <!-- Right -->
+			        <ul class="navbar-nav nav-flex-icons">
+			          <li class="nav-item">
+			            <a href="#" class="nav-link"
+			              target="_blank">
+			              <i class="#"></i>Cerrar Sesión
+			            </a>
+			          </li>
+			        </ul>
+
+			      </div>
+
+			    </div>
+			  </nav>
+			  <!-- Navbar -->
+    		');
+    }
+
+}
---
+++ resources/Gaby/private_area/users/forget.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Reset password</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body class="grey lighten-3">
+
+    <!--Main Navigation-->
+    <header>
+
+  <!-- Navbar -->
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark mdb-color scrolling-navbar">
+    <div class="container">
+
+      <!-- Brand -->
+      <a class="navbar-brand" href="#" target="_blank">
+        <strong>PopMovies</strong>
+      </a>
+
+      <!-- Collapse -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Links -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Left -->
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Registro
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" target="_blank">Acerca de</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://mdbootstrap.com/docs/jquery/getting-started/download/" target="_blank">Contáctanos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://mdbootstrap.com/education/bootstrap/" target="_blank">Preguntas frecuentes</a>
+          </li>
+        </ul>
+
+        <!-- Right -->
+        <ul class="navbar-nav nav-flex-icons">
+          <li class="nav-item">
+            <a href="form.php" class="nav-link"
+              target="_blank">
+              <i class="#"></i>Iniciar Sesión
+            </a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+    </header>
+    <!--Main Navigation-->
+
+    <!--Main layout-->
+    <main class="mt-5 pt-5">
+        <div class="container">
+
+            <!--Section: Post-->
+            <section class="mt-4">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-4 mb-4">
+
+                        <!--Card: Jumbotron-->
+                        <div class="card blue-gradient mb-4 wow fadeIn">
+
+                            <!-- Content -->
+                            <div class="card-body text-white text-center">
+
+                                <h4 class="mb-4">
+                                    <strong>Learn Bootstrap 4 with MDB</strong>
+                                </h4>
+                                <p>
+                                    <strong>Best & free guide of responsive web design</strong>
+                                </p>
+                                <p class="mb-4">
+                                    <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video
+                                        and written versions available. Create your own, stunning website.</strong>
+                                </p>
+                                <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-outline-white btn-md">Start free tutorial
+                                    <i class="fas fa-graduation-cap ml-2"></i>
+                                </a>
+
+                            </div>
+                            <!-- Content -->
+                        </div>
+                        <!--Card: Jumbotron-->
+
+                        <!--Card : Dynamic content wrapper-->
+                        <div class="card mb-4 text-center wow fadeIn">
+
+                            <div class="card-header">Do you want to get informed about new articles?</div>
+
+                            <!--Card content-->
+                            <div class="card-body">
+
+                                <!-- Default form login -->
+                                <form>
+
+                                    <!-- Default input email -->
+                                    <label for="defaultFormEmailEx" class="grey-text">Your email</label>
+                                    <input type="email" id="defaultFormLoginEmailEx" class="form-control">
+
+                                    <br>
+
+                                    <!-- Default input password -->
+                                    <label for="defaultFormNameEx" class="grey-text">Your name</label>
+                                    <input type="text" id="defaultFormNameEx" class="form-control">
+
+                                    <div class="text-center mt-4">
+                                        <button class="btn btn-info btn-md" type="submit">Sign up</button>
+                                    </div>
+                                </form>
+                                <!-- Default form login -->
+
+                            </div>
+
+                        </div>
+                        <!--/.Card : Dynamic content wrapper-->
+
+                        <!--Card-->
+                        <div class="card mb-4 wow fadeIn">
+
+                            <div class="card-header">Related articles</div>
+
+                            <!--Card content-->
+                            <div class="card-body">
+
+                                <ul class="list-unstyled">
+                                    <li class="media">
+                                        <img class="d-flex mr-3" src="https://mdbootstrap.com/img/Photos/Others/placeholder7.jpg" alt="Generic placeholder image">
+                                        <div class="media-body">
+                                            <a href="">
+                                                <h5 class="mt-0 mb-1 font-weight-bold">List-based media object</h5>
+                                            </a>
+                                            Cras sit amet nibh libero, in gravida nulla (...)
+                                        </div>
+                                    </li>
+                                    <li class="media my-4">
+                                        <img class="d-flex mr-3" src="https://mdbootstrap.com/img/Photos/Others/placeholder6.jpg" alt="An image">
+                                        <div class="media-body">
+                                            <a href="">
+                                                <h5 class="mt-0 mb-1 font-weight-bold">List-based media object</h5>
+                                            </a>
+                                            Cras sit amet nibh libero, in gravida nulla (...)
+                                        </div>
+                                    </li>
+                                    <li class="media">
+                                        <img class="d-flex mr-3" src="https://mdbootstrap.com/img/Photos/Others/placeholder5.jpg" alt="Generic placeholder image">
+                                        <div class="media-body">
+                                            <a href="">
+                                                <h5 class="mt-0 mb-1 font-weight-bold">List-based media object</h5>
+                                            </a>
+                                            Cras sit amet nibh libero, in gravida nulla (...)
+                                        </div>
+                                    </li>
+                                </ul>
+
+                            </div>
+
+                        </div>
+                        <!--/.Card-->
+
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+            </section>
+            <!--Section: Post-->
+
+        </div>
+    </main>
+    <!--Main layout-->
+
+    <!--Footer-->
+    <footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn">
+
+        <!--Call to action-->
+        <div class="pt-4">
+            <a class="btn btn-outline-white" href="https://mdbootstrap.com/docs/jquery/getting-started/download/" target="_blank" role="button">Download MDB
+                <i class="fas fa-download ml-2"></i>
+            </a>
+            <a class="btn btn-outline-white" href="https://mdbootstrap.com/education/bootstrap/" target="_blank" role="button">Start free tutorial
+                <i class="fas fa-graduation-cap ml-2"></i>
+            </a>
+        </div>
+        <!--/.Call to action-->
+
+        <hr class="my-4">
+
+        <!-- Social icons -->
+        <div class="pb-4">
+            <a href="https://www.facebook.com/mdbootstrap" target="_blank">
+                <i class="fab fa-facebook-f mr-3"></i>
+            </a>
+
+            <a href="https://twitter.com/MDBootstrap" target="_blank">
+                <i class="fab fa-twitter mr-3"></i>
+            </a>
+
+            <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
+                <i class="fab fa-youtube mr-3"></i>
+            </a>
+
+            <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
+                <i class="fab fa-google-plus-g mr-3"></i>
+            </a>
+
+            <a href="https://dribbble.com/mdbootstrap" target="_blank">
+                <i class="fab fa-dribbble mr-3"></i>
+            </a>
+
+            <a href="https://pinterest.com/mdbootstrap" target="_blank">
+                <i class="fab fa-pinterest mr-3"></i>
+            </a>
+
+            <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
+                <i class="fab fa-github mr-3"></i>
+            </a>
+
+            <a href="http://codepen.io/mdbootstrap/" target="_blank">
+                <i class="fab fa-codepen mr-3"></i>
+            </a>
+        </div>
+        <!-- Social icons -->
+
+        <!--Copyright-->
+        <div class="footer-copyright py-3">
+            © 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/" target="_blank"> MDBootstrap.com </a>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+    <!--/.Footer-->
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Initializations -->
+    <script type="text/javascript">
+        // Animations initialization
+        new WOW().init();
+    </script>
+</body>
+
+</html>
